8b969ae0e3fb9cb6ee302f5b254fc753c3ee69f5
chris shayo <chrisshayo4@gmail.com>
2024-12-20 22:28:02 +0300

    config: CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.setGlobalPrefix('eshop/api/');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
